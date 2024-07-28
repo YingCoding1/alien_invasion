bf86d8d9ab0a3cbe0be92297ddd6a36be9b95c95
Jessica <wyx01263@gmail.com>
2024-07-28 11:01:57 -0400

    add setting

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,13 +1,16 @@
 import sys
 import pygame
 
+from settings import Settings
+
 class AlienInvasion:
     """Overall class to manage game assets and behavior"""
 
     def __init__(self):
         pygame.init()
+        self.settings = Settings()
 
-        self.screen = pygame.display.set_mode((1200, 800))
+        self.screen = pygame.display.set_mode((self.settings.screen_width, self.settings.screen_height))
         pygame.display.set_caption("Alien Invasion")
         self.bg_color = (230, 230, 230)
 
